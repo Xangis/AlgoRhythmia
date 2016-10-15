4a2f817653003ec3b3dc4f3abd512637808f9356
Jason Champion <jchampion@zetacentauri.com>
2016-10-15 02:59:27 -0700

    Add samples to bundle in package command.

--- installer/package.sh
+++ installer/package.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-#cp -R ../Release/Patches Sigmatizm.app/Contents/Resources/
+cp -R ../samples AlgoRhythmia.app/Contents/Resources/
 codesign -f --deep -s "3rd Party Mac Developer Application: Jason Champion (G6PFF85X2R)" AlgoRhythmia.app --entitlements AlgoRhythmia.app/Contents/Resources/Entitlements.plist
 productbuild --sign "3rd Party Mac Developer Installer: Jason Champion (G6PFF85X2R)" --component AlgoRhythmia.app /Applications AlgoRhythmia.pkg
